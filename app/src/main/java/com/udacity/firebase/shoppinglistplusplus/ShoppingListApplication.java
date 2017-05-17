0c24f204b238dc53e186287a3c3c70d1d94774c5
Hasnain Baxamoosa <hbaxamoosa@gmail.com>
2017-05-17 11:17:48 -0500

    Initial commit

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ShoppingListApplication.java
@@ -0,0 +1,36 @@
+package com.udacity.firebase.shoppinglistplusplus;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.stetho.Stetho;
+import timber.log.Timber;
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+public class ShoppingListApplication extends android.app.Application {
+
+    private static Context context;
+
+    public static Context getAppContext() {
+        return ShoppingListApplication.context;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ShoppingListApplication.context = getApplicationContext();
+
+        //Including Jake Wharton's Timber logging library
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            Timber.v("Timber.plant(new Timber.DebugTree());");
+        } else {
+            // Timber.plant(new CrashReportingTree());
+        }
+
+        // Facebook Stetho
+        Stetho.initializeWithDefaults(this);
+    }
+}
